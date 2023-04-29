fa0ff256749f1815db38d60ac8099f3a4c2ece1e
Simon Dutton <simonpcdutton@gmail.com>
2023-04-29 02:52:07 -0400

    day 2: working full list of searched cities, formatting, gifs depending on the type of weather

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const API_KEY = `885f9c56756c8aaeed76d5b803228830`;
 
 const container = document.querySelector('.container');
+const errorText = document.querySelector('.error-text');
+//const list_container = document.querySelector('.container-for-list'); // for the list page
 let newHTML;
 
 const getFormData = async (e) => {
     e.preventDefault(); // don't reload the page
-    container.innerHTML = '' // set this to nothing to re-start
-
+    //container.innerHTML = '' // set this to nothing to re-start
+    errorText.innerHTML = ''
     console.log('getting form data');
     // while we are waiting, we print out a little loading message
     newHTML = document.createElement('h1');
     newHTML.innerText = 'Loading city...'
     // newHTML.className = 'loading'
-    container.append(newHTML);
+    
+    //container.append(newHTML);
+    errorText.append(newHTML);
 
     const city_name = e.target.location_input.value; // what the user has typed
 
@@ -37,7 +41,8 @@ const getLatLon = async (city) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    container.innerHTML = '' // reset the loading message, this happens after the awaits have finished
+    //container.innerHTML = '' // reset the loading message, this happens after the awaits have finished
+    errorText.innerHTML = ''
 
     if (data.length == 1) { // if length = 1 (success)
         return [data[0].lat, data[0].lon];
@@ -48,7 +53,8 @@ const getLatLon = async (city) => {
         } else { // if the city was not found in the api
             newHTML.innerText = 'City not found.';
         }
-        container.append(newHTML);
+        //container.append(newHTML);
+        errorText.append(newHTML);
         return null
     }
 
@@ -64,19 +70,35 @@ const getWeather = async (lat, lon) => {
     //newHTML.style.display = 'flex';
     //newHTML.style.justifyContent = 'center';
     //const date = new Date(data.dt*1000)
-    newHTML.innerHTML = `<div class="city-short-listing">
+
+    var d = new Date();
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    var nd = new Date(utc + (1000*data.timezone));
+    const timeString = nd.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+
+    newHTML.innerHTML = `<div class="city-short-listing ${(data.weather[0].main).toLowerCase()}">
                             <span class="location">${data.name}</span>
-                            <span class="current-temp">${Math.round(data.main.temp)}º</span>
+                            <span class="current-temp">${Math.round(data.main.temp)}º<a>x</a></span>
                             <span class="low-high">L:${Math.round(data.main.temp_min)}º H:${Math.round(data.main.temp_max)}º</span> 
-                            <span class="time-at-loc">${new Date(data.dt).getTime()}</span>
-                            <a class="clear">Clear</a>
+                            <span class="time-at-loc">${timeString}</span>
+                            <span class="type-of-weather">${data.weather[0].description}</span>
                         </div>`
 
     // we can get if something is day/night based on the third digit of the icon used
-    container.append(newHTML);
+    //container.appendChild(newHTML);
+    container.prepend(newHTML); // prepend adds to the top
+
+    //list_container.append(newHTML);
 }
 
 // we can also have a get hourly forecast!
 
-const form = document.getElementById('locationForm') // get the right form
-form.addEventListener('submit', getFormData)
+// const form = document.getElementById('locationForm') // get the right form
+// form.addEventListener('submit', getFormData)
+
+// const forms = document.getElementsByTagName('form')
+// console.log(forms)
+const forms = document.getElementsByTagName('form')
+console.log(forms)
+forms[0].addEventListener('submit', getFormData)
+//forms[1].addEventListener('submit', getFormData)
